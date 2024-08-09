d307b7d99736869b253185f2afa20cc7ea309c47
DmitriyRiabko <riabko97@gmail.com>
2024-08-09 20:46:37 +0300

    some fixes

--- src/app/api/ingredients/route.ts
+++ src/app/api/ingredients/route.ts
@@ -5,3 +5,6 @@ export async function GET() {
   const ingredients = await prisma.ingredient.findMany();
   return NextResponse.json(ingredients);
 }
+
+
+
--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
 import React from "react";
-import { Container } from "./container";
+import { Container, SearchInput } from ".";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
 
 interface Props {
   className?: string;
@@ -13,12 +14,18 @@ export const Header: React.FC<Props> = ({ className }) => {
   return (
     <header className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="Logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">So tasty</p>
+        <Link href={"/"}>
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="Logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">So tasty</p>
+            </div>
           </div>
+        </Link>
+
+        <div className="mx-10 flex-1 ">
+          <SearchInput />
         </div>
         <div className="flex items-center gap-3">
           <Button variant={"outline"} className="gap-2">
