5810665cd8af78d14737be1ceb0bf293ef2f47f5
DmitriyRiabko <riabko97@gmail.com>
2024-08-15 17:21:20 +0300

    new

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,57 @@
+import {
+  Container,
+  Filters,
+  ProductsGroupList,
+  Title,
+  TopBar,
+} from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      product: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+
+  const validCategories = categories.filter(
+    (category) => category.product.length > 0
+  );
+
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="All Pizza" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar categories={validCategories} />
+
+      <Container className="pb-14 mt-10">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {categories.map(
+                (category) =>
+                  category.product.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      products={category.product}
+                      categoryId={category.id}
+                    />
+                  )
+              )}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -2,29 +2,21 @@
 
 import { cn } from "@/lib/utils";
 import { useCategoryStore } from "@/store/category.store";
+import { Category } from "@prisma/client";
 import React from "react";
 
 interface Props {
+  items: Category[];
   className?: string;
 }
 
-const cats = [
-  { id: 1, name: "Pizza" },
-  { id: 2, name: "Combo" },
-  { id: 3, name: "Sides" },
-  { id: 4, name: "Cocktails" },
-  { id: 5, name: "Coffee" },
-  { id: 6, name: "Drinks" },
-  { id: 7, name: "Desserts" },
-];
-
-export const Categories: React.FC<Props> = ({ className }) => {
+export const Categories: React.FC<Props> = ({ items, className }) => {
   const categoryId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map((item) => (
+      {items?.map((item) => (
         <a
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
@@ -32,7 +24,7 @@ export const Categories: React.FC<Props> = ({ className }) => {
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
           href={`/#${item.name}`}
-          key={item.name}
+          key={item.id}
         >
           <button>{item.name}</button>
         </a>
