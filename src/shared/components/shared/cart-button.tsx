0c12857842add7d51a677ac9c8afca442ae239b2
DmitriyRiabko <riabko97@gmail.com>
2024-09-26 15:39:26 +0300

    added new

--- src/shared/components/shared/cart-button.tsx
+++ src/shared/components/shared/cart-button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "../ui/button";
 import { ArrowRight, ShoppingCart } from "lucide-react";
 import { cn } from "@/shared/lib/utils";
+import { CartDrawer } from "./cart-drawer";
 
 interface Props {
   className?: string;
@@ -9,14 +10,16 @@ interface Props {
 
 export const CartButton: React.FC<Props> = ({ className }) => {
   return (
-    <Button variant={"default"} className={cn("group relative", className)}>
-      <b>$ 9</b>
-      <span className="h-full w-[1px] bg-white/30 mx-3"></span>
-      <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-        <ShoppingCart className="size-4 relative" strokeWidth={2} />
-        <b>3</b>
-      </div>
-      <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
-    </Button>
+    <CartDrawer>
+      <Button variant={"default"} className={cn("group relative", className)}>
+        <b>$ 9</b>
+        <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+        <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+          <ShoppingCart className="size-4 relative" strokeWidth={2} />
+          <b>3</b>
+        </div>
+        <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+      </Button>
+    </CartDrawer>
   );
 };
--- src/shared/components/shared/cart-drawer.tsx
+++ src/shared/components/shared/cart-drawer.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetTitle,
+  SheetHeader,
+  SheetContent,
+  SheetFooter,
+} from "../ui/sheet";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
 
 interface Props {
-    className?: string;
+  className?: string;
 }
 
-export const CartDrawer: React.FC<Props> = ({ className }) => {
+export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
+  children,
+}) => {
   return (
-    <div className={className}></div>
+    <Sheet>
+      <SheetTrigger> {children} </SheetTrigger>
+      <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
+        <SheetHeader>
+          <SheetTitle>
+            In cart <span className="font-bold">3 items</span>
+          </SheetTitle>
+        </SheetHeader>
+
+        <SheetFooter className="mx-6 bg-white p-8">
+          <div className="w-full">
+            <div className="flex mb-4">
+              <span className="flex flex-1 text-lg text-neutral-500">
+                Total{" "}
+                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2"></div>
+              </span>
+
+              <span className="font-bold text-lg">{654} $</span>
+            </div>
+
+            <Link href={"/cart"}>
+              <Button type="submit" className="w-full h-12 text-base">
+                Make Order <ArrowRight className="w-5 ml-2" />
+              </Button>
+            </Link>
+          </div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
